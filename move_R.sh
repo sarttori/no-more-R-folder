a02270779cadc9edea7ab18d8c182fd33196aa34
sarttori <171109581+sarttori@users.noreply.github.com>
2026-05-10 00:30:51 -0300

    Refactor comments in move_R.sh for clarity
    Updated comments for clarity and removed unnecessary lines.

--- move_R.sh
+++ move_R.sh
@@ -1,8 +1,3 @@
-# This script will (i) make a folder for R packages to live 
-# in from now on (in .local/share), (ii) warn R about this 
-# change, (iii) move current packages from one to another 
-# and (iv) remove the vexing folder it creates in /home.
-
 mkdir -p ~/.local/share/R
 
 echo 'R_LIBS_USER="~/.local/share/R/%p-library/%v"' >> ~/.Renviron
@@ -11,6 +6,6 @@ mv ~/R/* ~/.local/share/R/ 2>/dev/null
 
 rm -rf ~/R
 
-# To be absolute sure it worked, enter terminal, type R and Enter;
-# then run this command: .libPaths()
+# To be absolute sure it worked: enter the terminal, type R
+# to enter the environment,o then run this command: .libPaths()
 # The first output path must be the one set above. 
